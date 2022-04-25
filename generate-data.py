f1fa792499ec1d68bef0e820e2c12abe818c4aa7
Owen Rocchi <orocchi27@googlemail.com>
2022-04-25 17:55:32 -0400

    added pulling stats

--- generate-data.py
+++ generate-data.py
@@ -1,23 +1,45 @@
+# https://github.com/swar/nba_api
+# https://github.com/swar/nba_api/tree/master/docs/nba_api/stats/endpoints
 
 import json
-from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.endpoints import shotchartdetail, playercareerstats
 import pandas as pd
 
-response = shotchartdetail.ShotChartDetail( 
+def json_to_df(response):
+
+    # Turn json response into object
+    content = json.loads(response.get_json())
+
+    # Transform content to dataframe
+    results = content["resultSets"][0]
+    headers = results["headers"]
+    rows = results["rowSet"]
+    df = pd.DataFrame(rows)
+    df.columns = headers
+
+    return df
+
+if __name__ == "__main__":
+
+    response = shotchartdetail.ShotChartDetail (
         team_id=0,
         player_id=1630567, # Scottie Barnes
         season_nullable="2021-22",
         season_type_all_star="Regular Season"
-)
+    )
+    # Transform to df
+    ShotChart_df = json_to_df(response)
+    # Convert to CSV
+    ShotChart_df.to_csv("barnes-shotchartdetail.csv", index=False)
 
-content = json.loads(response.get_json())
+    response = playercareerstats.PlayerCareerStats (
+        player_id=1630567, # Scottie Barnes
 
-# Transform content to dataframe
-results = content["resultSets"][0]
-headers = results["headers"]
-rows = results["rowSet"]
-df = pd.DataFrame(rows)
-df.columns = headers
+    )
+    # Transform to df
+    CommonInfo_df = json_to_df(response)
+    # Convert to CSV
+    CommonInfo_df.to_csv("barnes-careerstats.csv", index=False)
 
-# Convert to CSV
-df.to_csv("barnes-shotchartdetail.csv", index=False)
+
+
